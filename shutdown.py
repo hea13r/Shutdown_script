09ba86403807783fc2dd4413a6fdea8455c8ce54
hea13r <162923949+hea13r@users.noreply.github.com>
2024-03-29 14:19:16 -0500

    Update shutdown.py

--- shutdown.py
+++ shutdown.py
@@ -6,6 +6,8 @@ def ipCheck(x):
     pingCheck = subprocess.run(['ping', x, '-c 1', '-W 1'], stdout=subprocess.DEVNULL)
     if "returncode=0" in str(pingCheck):
         return x
+    else:
+        pass
 
 def ipChecker(x):
     shutdownList = []
@@ -32,21 +34,25 @@ if checkAgain.lower() != 'yes':
 
 while len(shutdownList) != 0:
     for i in shutdownList:
+        if i == None: #Sometimes the ipChecker will return a None value which breaks this loop, this fixes that
+            shutdownList.remove(None)
+            continue
         print(f"Attempting to shutdown {shutdownList[i]}...")
         subprocess.run(['ssh', i, 'shutdown -h now'], stdout=subprocess.DEVNULL)
-        time.sleep(10)
-        if not ipCheck(i):
+        time.sleep(10) #Gives time for the IP to shutdown
+        if not ipCheck(i): #Checks to make sure IP is shutdown before removing from list
             shutdownList.remove(i)
 
 print("One final check to make sure everything is off...")
 finalCheck = ipChecker(ipList)
-finalAnswer = input(f"These are the IPs I found still active: {finalCheck}.\nIf blank type yes to continue, otherwise consider starting over.\n")
+finalAnswer = input(f"These are the IPs I found still active: {finalCheck}.\nIf blank or [None] type yes to continue, otherwise consider starting over.\n")
 if finalAnswer.lower() != 'yes':
     exit(0)
 
 print("I will now shutdown the control-plane...")
 subprocess.run(['ssh', 'ip', 'shutdown -h now'], stdout=subprocess.DEVNULL) #you must enter the ip
-time.sleep(10)
+time.sleep(10) #gives time for it to shutdown before shutting down your machine
+               #also gives you time to remember your machine is about to be shutdown
 
 print("I will now shut myself down...")
 subprocess.run(['shutdown', '-h', 'now'])
